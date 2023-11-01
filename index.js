d05879afb0ac97712cd8b23bd5a0d59ca6102c0d
Tejas Nampalli <TSNampalli@gmail.com>
2023-11-01 12:33:04 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 =31
+let num2= 2
+const multiply = (num1 *num2)
 
-// Write your code here
+//Next Request//
+
+const random = Math.floor(Math.random() * 10) + 1
+
+//Next Request//
+const num3 = 8
+const num4 = 2
+const mod = (num3/num4)
+
+//Next Request//
+
+const max = Math.max(1,2,3,4,5,20)
